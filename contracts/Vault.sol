7799200f63900757bfbcc4ffa294ee9bff2b7a7f
JimLynchCodes <mrdotjim@gmail.com>
2022-04-19 13:12:15 -0400

    trying to debug failing test

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -8,7 +8,8 @@ contract Vault {
 
     Prime primeInterface;
     PUSD pusdInterface;
-    mapping(address => uint) public balances;
+    mapping(address => uint) public primeBalances;
+    mapping(address => uint) public pusdBalances;
     mapping(address => uint) public depositTime;
 
     constructor(address _addressPrime, address _addressPusd){
@@ -19,28 +20,28 @@ contract Vault {
     function deposit(uint amount) public {
         primeInterface.allowance(msg.sender, address(this));
         primeInterface.transferFrom(msg.sender, address(this), amount);
-        balances[msg.sender] += amount;
+        primeBalances[msg.sender] += amount;
         depositTime[msg.sender] = block.timestamp;
     }
 
     function interestCalc(address _address) public view returns(uint) {
         uint diff = block.timestamp - depositTime[_address];
-        uint principal = balances[msg.sender];
+        uint principal = primeBalances[msg.sender];
         uint secsPerYear = 60 *60 *24 *365;
         uint interest = (principal * 1 * diff)/1e2 ;
         uint interestDue = interest / secsPerYear;
         return interestDue;
     }
 
-    function checkBalance() public view returns(uint) {
-        return balances[msg.sender] + interestCalc(msg.sender);
+    function checkBalance() public view returns(uint, uint) {
+        return (primeBalances[msg.sender], primeBalances[msg.sender]);
     }
     
     // function withdraw(uint amount) public {
     //     uint new_balance = checkBalance();
     //     depositTime[msg.sender] = block.timestamp;
-    //     balances[msg.sender] = new_balance;
-    //     balances[msg.sender] -= amount;
+    //     primeBalances[msg.sender] = new_balance;
+    //     primeBalances[msg.sender] -= amount;
         
     //     pusdInterface.approve(address(this), amount);
     //     pusdInterface.transferFrom(from, to, amount);
--- test/sample.test.js
+++ test/sample.test.js
@@ -35,82 +35,94 @@ describe("Prime ERC20 Interactions", function () {
 
   it("Should test that owner recieves 1000 Prime tokens, others start with 0", async function () {
     const alice_address = alice.address;
-    alice_balance = await primeContract.balanceOf(alice_address)
+    alice_balance = await primeContract.balanceOf(alice.address)
     expect(alice_balance.toString()).to.equal("1000");
 
-    bob_balance = await primeContract.balanceOf(bob_address)
+    bob_balance = await primeContract.balanceOf(bob.address)
     expect(bob_balance.toString()).to.equal("0");
 
-    charlie_balance = await primeContract.balanceOf(charlie_address)
+    charlie_balance = await primeContract.balanceOf(charlie.address)
     expect(charlie_balance.toString()).to.equal("0");
 
-    david_balance = await primeContract.balanceOf(david_address)
+    david_balance = await primeContract.balanceOf(david.address)
     expect(david_balance.toString()).to.equal("0");
   });
 
   it("Should test that owner recieves 1000 PUSD tokens", async function () {
-    const bob_address = bob.address;
-    bob_balance = await pusdContract.balanceOf(bob_address);
+    bob_balance = await pusdContract.balanceOf(bob.address);
     expect(bob_balance.toString()).to.equal("1000");
+
+    //TODO check starting balances of other users
   });
 
   describe('check balances of deposits', () => {
 
-    beforeEach(() => {
+    beforeEach(async () => {
 
+      console.log(await primeContract.balanceOf(alice.address))
+      
+      await primeContract.connect(alice).increaseAllowance(vaultContract.address, 100);
+      
+      console.log(await primeContract.balanceOf(vaultContract.address))
+      
       // charlie (not the owner) starts with 100 PRIME
-      pusdContract.transferFrom(alice.address, charlie.address, 100);
+      await primeContract.connect(alice).transferFrom(alice.address, vaultContract.address, 100);
+      
+      console.log(await primeContract.balanceOf(vaultContract.address))
+    })
 
-    }
+    it.only('keeps track of deposited PRIME and earned PUSD interest', async () => {
 
-    it('keeps track of deposited PRIME and earned PUSD interest', () => {
+      // const charlie_prime_starting_balance = await primeContract.balanceOf(charlie.address);
+      // console.log(charlie_prime_starting_balance);
 
-      const [charlie_prime_balance_before_deposit,
-        charlie_pusd_balance_before_deposit] = await vaultContract.connect(charlie).balances(charlie.address);
-      console.log(charlie_prime_balance);
+      // const [charlie_prime_balance_before_deposit,
+      //   charlie_pusd_balance_before_deposit] = await vaultContract.connect(charlie).checkBalance();
+      // console.log(charlie_prime_balance_before_deposit);
+      // console.log(charlie_pusd_balance_before_deposit);
 
-      expect(charlie_prime_balance_before_deposit).to.equal(0)
-      expect(charlie_pusd_balance_before_deposit).to.equal(0)
+      // expect(charlie_prime_balance_before_deposit).to.equal(0)
+      // expect(charlie_pusd_balance_before_deposit).to.equal(0)
 
-      await primeContract.connect(charlie).approve(vaultContract.address, 100);
-      await vaultContract.connect(charlie).deposit(100);
+      // await primeContract.connect(charlie).approve(vaultContract.address, 100);
+      // await vaultContract.connect(charlie).deposit(100);
 
-      const [charlie_prime_balance_after_deposit,
-        charlie_pusd_balance_after_deposit] = await vaultContract.connect(charlie).balances(charlie.address);
-      console.log(charlie_prime_balance);
+      // const [charlie_prime_balance_after_deposit,
+      //   charlie_pusd_balance_after_deposit] = await vaultContract.connect(charlie).checkBalance();
+      // // console.log(charlie_prime_balance);
 
-      expect(charlie_prime_balance_before_deposit).to.equal(100)
-      expect(charlie_pusd_balance_before_deposit).to.equal(0)
-
-
-      await ethers.provider.send("evm_increaseTime", [24 * 60 * 60 * 365]);
-      await ethers.provider.send("evm_mine");
-
-      const [charlie_prime_balance_after_one_epoch,
-        charlie_pusd_balance_after_one_epoch] = await vaultContract.connect(charlie).balances(charlie.address);
-      console.log(charlie_prime_balance);
-
-      expect(charlie_prime_balance_before_deposit).to.equal(100)
-      expect(charlie_pusd_balance_before_deposit).to.equal(1)
+      // expect(charlie_prime_balance_before_deposit).to.equal(100)
+      // expect(charlie_pusd_balance_before_deposit).to.equal(0)
 
 
-      await ethers.provider.send("evm_increaseTime", [24 * 60 * 60 * 365]);
-      await ethers.provider.send("evm_mine");
+      // await ethers.provider.send("evm_increaseTime", [24 * 60 * 60 * 365]);
+      // await ethers.provider.send("evm_mine");
 
-      const [charlie_prime_balance_after_two_epoch,
-        charlie_pusd_balance_after_two_epoch] = await vaultContract.connect(charlie).balances(charlie.address);
-      console.log(charlie_prime_balance);
+      // const [charlie_prime_balance_after_one_epoch,
+      //   charlie_pusd_balance_after_one_epoch] = await vaultContract.connect(charlie).checkBalance();
+      // // console.log(charlie_prime_balance);
 
-      expect(charlie_prime_balance_before_deposit).to.equal(100)
-      expect(charlie_pusd_balance_before_deposit).to.equal(2)
+      // expect(charlie_prime_balance_before_deposit).to.equal(100)
+      // expect(charlie_pusd_balance_before_deposit).to.equal(1)
+
+
+      // await ethers.provider.send("evm_increaseTime", [24 * 60 * 60 * 365]);
+      // await ethers.provider.send("evm_mine");
+
+      // const [charlie_prime_balance_after_two_epoch,
+      //   charlie_pusd_balance_after_two_epoch] = await vaultContract.connect(charlie).checkBalance();
+      // // console.log(charlie_prime_balance);
+
+      // expect(charlie_prime_balance_before_deposit).to.equal(100)
+      // expect(charlie_pusd_balance_before_deposit).to.equal(2)
 
     })
 
   })
 
-  describe('users are able to withdraw prime & pusd', () => {
+  xdescribe('users are able to withdraw prime & pusd', () => {
 
-    beforeEach(() => {
+    beforeEach(async () => {
       await primeContract.connect(charlie).approve(vaultContract.address, 100);
       await vaultContract.connect(charlie).deposit(100);
  
@@ -124,7 +136,7 @@ describe("Prime ERC20 Interactions", function () {
       expect(charlie_pusd_balance_before_deposit).to.equal(2)
     })
 
-    it('withdraws correctly', () => {
+    xit('withdraws correctly', async () => {
 
       await vaultContract.connect(charlie).withdraw(100);
       
@@ -148,7 +160,7 @@ describe("Prime ERC20 Interactions", function () {
 
   })
 
-  it("should test that the Vault interest function works", async function () {
+  xit("should test that the Vault interest function works", async function () {
 
     await primeContract.connect(alice).approve(vaultContract.address, 100);
     await vaultContract.connect(alice).deposit(100);
